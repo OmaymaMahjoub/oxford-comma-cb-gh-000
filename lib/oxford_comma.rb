d73563f002aac9a14a90ab324bc066bb0e41c522
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-08 19:46:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   ch=array.join(", ")
   ar2=ch.split(", #{array[-1]}")
   ch=ar2.join(", ")
-  ch<<("and #{array[-1]}")
+  ch<<(" and #{array[-1]}")
   puts ch
 end
 oxford_comma(["test","tes2","gfh2"])
