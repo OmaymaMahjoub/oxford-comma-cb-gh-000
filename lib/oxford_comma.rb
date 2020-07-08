828f5d805e77e824c0bbb2fb6bfc8a68aef67984
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-08 19:51:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,13 @@
 def oxford_comma(array)
-  ch=array.join(", ")
-  ar2=ch.split(", #{array[-1]}")
-  ch=ar2.join(", ")
-  ch<<(", and #{array[-1]}")
+  if array.size<=2
+    ch=array.join(" and ")
+  elsif array.size=1
+    return array[0]
+  else 
+    ch=array.join(", ")
+    ar2=ch.split(", #{array[-1]}")
+    ch=ar2.join(", ")
+    ch<<(", and #{array[-1]}")
+  end
   return  ch
 end
